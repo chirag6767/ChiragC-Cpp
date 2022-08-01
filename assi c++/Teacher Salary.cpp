311ce9f48272f35d296cd7d6c75665455521b631
chirag6767 <sudrachirag0707@gmail.com>
2022-08-01 13:09:41 +0530

    teacher Salary

--- assi c++/Teacher Salary.cpp
+++ assi c++/Teacher Salary.cpp
@@ -1,9 +1,3 @@
-/*Create a class person having members name and age. 
-Derive a class student having member percentage. 
-Derive another class teacher having member salary. 
-Write necessary member function to initialize, read and write data. 
-Write also Main function (Multiple Inheritance)*/
-
 #include<iostream>
 using namespace std;
 
