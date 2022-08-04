c4f3550cb3147e11019f6cb58cc7247de35c3823
chirag6767 <sudrachirag0707@gmail.com>
2022-08-04 11:34:48 +0530

    swap number using template

---
+++ assi c++/swap number using template.cpp
@@ -0,0 +1,23 @@
+//3O . Write a program of to swap the two values using templates.
+
+#include<iostream>
+using namespace std;
+
+template <typename T>
+T chirag(T a, T b)
+{
+	T c;
+	cout<<"Before Swap A = "<<a<<"\tB = "<<b<<endl;
+	c=a;
+	a=b;
+	b=c;
+	cout<<"After Swap A = "<<a<<"\tB = "<<b<<endl;
+	return 0;
+}
+	
+int main()
+{
+	
+	 chirag(10, 20);
+	return 0;
+}
