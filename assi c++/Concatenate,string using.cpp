124e4e193b571d46a45e93cf6f31175eb4a8eefc
chirag6767 <109646938+chirag6767@users.noreply.github.com>
2023-11-23 17:52:45 +0530

    Update Concatenate,string using.cpp

--- assi c++/Concatenate,string using.cpp
+++ assi c++/Concatenate,string using.cpp
@@ -4,10 +4,10 @@ using namespace std;
 class STRING
 {
    public:
-        char n1[20];
-		char n2[25];
+        char n1[30];
+		char n2[35];
 		
-   STRING(char str1[20],char str2[25])
+   STRING(char str1[30],char str2[35])
    { 
        strcpy(n1,str1);
        strcpy(n2,str2);
@@ -22,8 +22,8 @@ class STRING
 
 int main()
 {
-	char str1[20] = "Chirag";
-	char str2[25] = "Sudra";
+	char str1[30] = "Chirag";
+	char str2[35] = "Sudra";
 	STRING n(str1,str2);
 	+n;
 	return 0;
